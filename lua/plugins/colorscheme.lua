e1596c8e901526f2e56923dad04cf7599b557f4d
rupture-ttv <mel.dommer@gmail.com>
2023-10-22 14:08:31 -0500

    new nvim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "rmehri01/onenord.nvim",
+    lazy = false,
+    opts = {
+      theme = "dark",
+      fade_nc = true,
+      diable = {
+        background = true,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onenord",
+    },
+  },
+}
